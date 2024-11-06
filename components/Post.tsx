1a829003f53d635e6a96843dba2e22f4455f2baf
Nengak Dakup <nengakdakup@gmail.com>
2024-11-06 04:14:10 +0100

    Add profile image

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,15 +1,20 @@
-import { ShareIcon } from 'lucide-react'
+import { PlusIcon, ShareIcon } from 'lucide-react'
 import { MessageSquare } from 'lucide-react'
 import { ThumbsUp } from 'lucide-react'
 import React from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
 
 const Post = ({ post }: { post: any }) => {
   return (
     <div key={post} className="rounded-lg border bg-card p-4 shadow-sm space-y-4">
       <div className="flex items-center space-x-3">
-        <div className="w-12 h-12 rounded-full bg-muted" />
+        <div className="w-12 h-12 rounded-full bg-muted">
+          <Image src="/assets/images/user.png" width={48} height={48} alt='user profile image' className='w-12 h-12 rounded-full' />
+        </div>
         <div>
-          <h3 className="font-semibold">User Name</h3>
+          <h3 className="font-semibold">Abu Joy . <span className='text-primary inline-flex underline text-xs cursor-pointer'>Follow <PlusIcon className='w-4 h-4' /></span></h3>
+          <p className='text-sm text-muted-foreground'>Chief Marketing Officer @ Binance</p>
           <p className="text-sm text-muted-foreground">Posted 2h ago</p>
         </div>
       </div>
@@ -21,18 +26,18 @@ const Post = ({ post }: { post: any }) => {
         <img src="https://media.istockphoto.com/id/521747273/vector/work-for-life.jpg?s=612x612&w=0&k=20&c=bLjt357WizASD-zcNYiaSqI8JeQBCda2ovuOgDmsXi8=" alt="Post Image 4" className="w-full h-auto" />
       </div>
       <div className="flex justify-between items-center mt-4">
-        <button className="text-primary hover:text-primary-foreground">
+        <Button variant="ghost" className="text-primary hover:text-primary-foreground">
           <ThumbsUp className="w-5 h-5" />
           <span className="ml-2">Like</span>
-        </button>
-        <button className="text-primary hover:text-primary-foreground">
+        </Button>
+        <Button variant="ghost" className="text-primary hover:text-primary-foreground">
           <MessageSquare className="w-5 h-5" />
           <span className="ml-2">Comment</span>
-        </button>
-        <button className="text-primary hover:text-primary-foreground">
+        </Button>
+        <Button variant="ghost" className="text-primary hover:text-primary-foreground">
           <ShareIcon className="w-5 h-5" />
           <span className="ml-2">Share</span>
-        </button>
+        </Button>
       </div>
     </div>
   )
